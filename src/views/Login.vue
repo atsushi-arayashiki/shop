ca7c1c0c00bf1c5061fe5244204201aa6bff676f
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-05-29 17:51:37 +0900

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,19 +4,42 @@
     <div class="login">
       <p class="login-title">ログイン</p>
       <div class="rogin-form">
-        <input placeholder="Email">
-        <input placeholder="Password">
+        <input placeholder="user_name" type="text" v-model="user_name">
+        <input placeholder="Password" type="text" v-model="password">
       </div>
-      <button class="">ログインする</button>
+      <button @click="auth">ログインする</button>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
+import axios from "axios";
 export default {
   components: {
     Header
+  },
+  data(){
+    return {
+      user_name: "",
+      password: ""
+    };
+  },
+  methods: {
+    auth() {
+      axios
+      .get("https://shielded-ridge-86495.herokuapp.com/api/users",{
+       user_name: this.user_name,
+       password: this.password
+      })
+      .then(response => {
+          console.log(response);
+          this.$router.replace("/");
+        })
+        .catch(error => {
+          alert(error);
+        });
+    }
   }
 }
 </script>
