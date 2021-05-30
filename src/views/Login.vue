6215fc9bbefc806872aa4980ba86978c7d9984ea
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-05-30 10:20:37 +0900

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,13 +7,41 @@
         <input placeholder="user_name" type="text" v-model="user_name">
         <input placeholder="Password" type="text" v-model="password">
       </div>
-      <button >ログインする</button>
+      <button @click="auth">ログインする</button>
     </div>
   </div>
 </template>
 
 <script>
-
+ import Header from "../components/Header";
+import axios from "axios";
+export default {
+  components: {
+    Header
+  },
+  data(){
+    return {
+      user_name: "",
+      password: ""
+    };
+  },
+  methods: {
+    auth() {
+      axios
+      .get("https://shielded-ridge-86495.herokuapp.com/api/users",{
+       user_name: this.user_name,
+       password: this.password
+      })
+      .then(response => {
+          console.log(response);
+          this.$router.replace("/");
+        })
+        .catch(error => {
+          alert(error);
+        });
+    }
+  }
+}
 </script>
 
 <style>
