e8c68a4b0fbd86d529358047b2400bb743fa8511
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-06-18 19:51:25 +0900

    test

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
     <Header />
-    <p>{{ shop_image }}</p>
-    <p>{{ shop_name }}</p>
-    <p>{{ shop_area }}</p>
-    <p>{{ shop_genre }}</p>
+    <table>
+      <tr v-for="item in shop_list" :key="item">
+      <p>{{ item.shop_image }}</p>
+      <p>{{ item.shop_name }}</p>
+      <p>{{ item.shop_area }}</p>
+      <p>{{ item.shop_genre }}</p>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -17,21 +21,27 @@ export default {
   },
   data() {
     return {
-      shop_image: "",
-      shop_name: "",
-      shop_area: "",
-      shop_genre: ""
+      shop_list: [
+      {shop_image: ""},
+      {shop_name: ""},
+      {shop_area: ""},
+      {shop_genre: ""},
+      ]
+      
     };
   },
-  async created() {
+  methods: {
+    async created() {
     const item = await axios.get(
-      `http://127.0.0.1:8000/api/shops`,
+      `http://127.0.0.1:8000/api/shops`
     );
-    const shop_list = item.data;
+    this.shop_list = item.data.data;
+    const shop_list = item;
     this.shop_image = shop_list.shop_image;
     this.shop_name = shop_list.shop_name;
     this.shop_area = shop_list.shop_area;
     this.shop_genre = shop_list.shop_genre;
+  }
   }
   };
 </script>
